471893391887b4202fb164249719cf3ecdfa3af9
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-19 14:46:28 -0400

    8080

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
