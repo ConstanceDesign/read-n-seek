fd3374aa5a1ead2345006be9e2ba4e9f101af5bf
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-19 12:20:05 -0400

    topology

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {}
+);
 
 module.exports = mongoose.connection;
